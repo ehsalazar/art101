3e3c81b220881044a48619ba00c11be21f23b3b7
Élan Salazar <ehsalazar75@gmail.com>
2021-01-27 15:48:48 -0800

    Declared and initialized variables and then output their values

---
+++ lab5/js/lab.js
@@ -0,0 +1,28 @@
+// Project: Lab 5: Data Types and Variables
+// Author: Élan Salazar & Selena Hyon
+// Created: January 27, 2021
+
+//declare variables
+var make = "Honda";
+var model = "Element";
+var color = "Blue";
+var year = 2006;
+var ownIt = false;
+
+// calculate
+var age = 2021 - year;
+
+//output
+// https://www.w3docs.com/snippets/javascript/how-to-do-string-interpolation-in-javascript.html
+document.writeln(`Make: ${make}<br>`);
+document.writeln(`Model: ${model}<br>`);
+document.writeln(`Color: ${color}<br>`);
+document.writeln(`Year: ${year}<br>`);
+document.writeln(`Age: ${age} years<br>`);
+
+// Section Exercise - January 26, 2021
+// var textAnalysis = "Glad you're here!";
+// // console.log(textAnalysis);
+// textAnalysis += " Let's explore."
+// // console.log(textAnalysis);
+// document.writeln(textAnalysis);
