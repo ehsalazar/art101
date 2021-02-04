f73284987ad92d40af0ab49eecc1e916ccbb4122
Élan Salazar <ehsalazar75@gmail.com>
2021-02-03 16:45:28 -0800

    Create a sectionD directory for section exercises

---
+++ sectionD/js/script.js
@@ -0,0 +1,11 @@
+// Section D - February 2, 2021
+// exploring functions and scope
+
+console.log("JS loaded!")
+
+function comp(x,y){
+    return x > y;
+}
+
+console.log(comp(2,1));
+console.log(comp(8,31));
