d2d90c22ceac20e596f9ac7523a6122d845f69fe
Élan Salazar <ehsalazar75@gmail.com>
2021-02-04 17:57:21 -0800

    Added new section exercises for 02.04.21

--- sectionD/js/script.js
+++ sectionD/js/script.js
@@ -1,11 +1,31 @@
 // Section D - February 2, 2021
+// exploring functions, anonymous functions, and callbacks
+
+// create a function that takes two parameters, a firstname and a lastname, and then returns a sentence using them.
+// example return: My first name is (firstname) and my last name is (lastname).
+function userInfo(first, last){
+    return `My first name is ${first} and my last name is ${last}.`
+}
+
+console.log(userInfo("Elan","Salazar"));
+
+var funFunc = function(){
+    console.log("Hi!");
+};
+
+setTimeout(funFunc,3000);
+
+//anonymous functions
+setTimeout(function(){console.log("Hi!");},5000);
+
+// Section D - February 2, 2021
 // exploring functions and scope
 
-console.log("JS loaded!")
+// console.log("JS loaded!")
 
 function comp(x,y){
     return x > y;
 }
 
-console.log(comp(2,1));
-console.log(comp(8,31));
+// console.log(comp(2,1));
+// console.log(comp(8,31));
