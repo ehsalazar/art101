eff6318fe443af9d955116c9477f80083798d532
Élan Salazar <ehsalazar75@gmail.com>
2021-02-09 14:51:07 -0800

    Created Lab 9 directories and files

---
+++ lab9/js/lab.js
@@ -0,0 +1,19 @@
+// Project: Lab 9: JavaScript for the Web
+// Author: Élan Salazar
+// Created: February 10, 2021
+
+// https://www.w3schools.com/jsref/dom_obj_all.asp
+
+// Use getElementById() to find your output <div> and assign it to a variable outputEl
+
+// Create a new element with document.createElement("p") and assign it to a variable new1El
+
+// Change the html attribute of new1El to say something new.
+
+// Create another new element and assign it to a variable new2El
+
+// Change the html attribute of new2El to say something else.
+
+// Append both new elements one at a time using appendChild() to your output <div>
+
+// Change the css attributes of at least two elements of your page
