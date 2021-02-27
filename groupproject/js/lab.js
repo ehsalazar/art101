f8ab2e9edbc1bf8e0e6aef87a207f52231c48171
Élan Salazar <ehsalazar75@gmail.com>
2021-02-27 14:51:48 -0800

    Added county specific information for San Francisco, San Mateo, Santa Clara, and Santa Cruz

--- groupproject/js/lab.js
+++ groupproject/js/lab.js
@@ -2,16 +2,7 @@
 // Author: Élan Salazar
 // Date: February 25, 2021
 
-
-// function toggleCounties(){
-//   var countyInfo = ["#alameda","#contracosta","#monterey","#sanbenito","#sanfrancisco","#sanmateo","#santaclara","#santacruz"];
-//   for (var i = 0; i<countyInfo.length; i++){
-//     $(countyInfo[i]).toggleClass("hidden");
-//   }
-// }
-//
-// $("#alameda_button").click(toggleCounties)
-
+//button group
 $("#alameda_button").click(function(){$("#alameda").toggleClass("hidden")})
 $("#contracosta_button").click(function(){$("#contracosta").toggleClass("hidden")})
 $("#monterey_button").click(function(){$("#monterey").toggleClass("hidden")})
@@ -21,18 +12,42 @@ $("#sanmateo_button").click(function(){$("#sanmateo").toggleClass("hidden")})
 $("#santaclara_button").click(function(){$("#santaclara").toggleClass("hidden")})
 $("#santacruz_button").click(function(){$("#santacruz").toggleClass("hidden")})
 
+// Alameda info buttons
 $("#testing_button_alameda").click(function(){$("#testing_alameda").toggleClass("unhideTest")});
 $("#vaccination_button_alameda").click(function(){$("#vaccination_alameda").toggleClass("unhideVaccine")});
 $("#additional_button_alameda").click(function(){$("#additional_alameda").toggleClass("unhideAdditional")});
 
+// Contra Costa info buttons
 $("#testing_button_contracosta").click(function(){$("#testing_contracosta").toggleClass("unhideTest")});
 $("#vaccination_button_contracosta").click(function(){$("#vaccination_contracosta").toggleClass("unhideVaccine")});
 $("#additional_button_contracosta").click(function(){$("#additional_contracosta").toggleClass("unhideAdditional")});
 
+// Monterey info buttons
 $("#testing_button_monterey").click(function(){$("#testing_monterey").toggleClass("unhideTest")});
 $("#vaccination_button_monterey").click(function(){$("#vaccination_monterey").toggleClass("unhideVaccine")});
 $("#additional_button_monterey").click(function(){$("#additional_monterey").toggleClass("unhideAdditional")});
 
+// San Benito info buttons
 $("#testing_button_sanbenito").click(function(){$("#testing_sanbenito").toggleClass("unhideTest")});
 $("#vaccination_button_sanbenito").click(function(){$("#vaccination_sanbenito").toggleClass("unhideVaccine")});
 $("#additional_button_sanbenito").click(function(){$("#additional_sanbenito").toggleClass("unhideAdditional")});
+
+// San Francisco info buttons
+$("#testing_button_sanfrancisco").click(function(){$("#testing_sanfrancisco").toggleClass("unhideTest")});
+$("#vaccination_button_sanfrancisco").click(function(){$("#vaccination_sanfrancisco").toggleClass("unhideVaccine")});
+$("#additional_button_sanfrancisco").click(function(){$("#additional_sanfrancisco").toggleClass("unhideAdditional")});
+
+// San Mateo info buttons
+$("#testing_button_sanmateo").click(function(){$("#testing_sanmateo").toggleClass("unhideTest")});
+$("#vaccination_button_sanmateo").click(function(){$("#vaccination_sanmateo").toggleClass("unhideVaccine")});
+$("#additional_button_sanmateo").click(function(){$("#additional_sanmateo").toggleClass("unhideAdditional")});
+
+// Santa Clara info buttons
+$("#testing_button_santaclara").click(function(){$("#testing_santaclara").toggleClass("unhideTest")});
+$("#vaccination_button_santaclara").click(function(){$("#vaccination_santaclara").toggleClass("unhideVaccine")});
+$("#additional_button_santaclara").click(function(){$("#additional_santaclara").toggleClass("unhideAdditional")});
+
+// Santa Cruz info buttons
+$("#testing_button_santacruz").click(function(){$("#testing_santacruz").toggleClass("unhideTest")});
+$("#vaccination_button_santacruz").click(function(){$("#vaccination_santacruz").toggleClass("unhideVaccine")});
+$("#additional_button_santacruz").click(function(){$("#additional_santacruz").toggleClass("unhideAdditional")});
